d4bec0a4574618e78406d38d998b428da57dd084
ShaoxinGu <853674765@qq.com>
2020-01-15 20:55:48 +0800

    Reformatted.

--- src/ldo.c
+++ src/ldo.c
@@ -159,8 +159,7 @@ int luaD_rawrunprotected(lua_State *L, Pfunc f, void *ud)
 	lj.status = LUA_OK;
 	lj.previous = L->errorJmp; /* chain new error handler */
 	L->errorJmp = &lj;
-	LUAI_TRY(L, &lj,
-			 (*f)(L, ud););
+	LUAI_TRY(L, &lj, (*f)(L, ud););
 	L->errorJmp = lj.previous; /* restore old error handler */
 	L->nCcalls = oldnCcalls;
 	return lj.status;
